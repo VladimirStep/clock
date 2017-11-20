bd135664903b99824fabc168d2f2750b96ed2d4c
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-20 15:30:30 +0200

    Refactor redusers. Add function to remove array elements.

--- src/redusers.js
+++ src/redusers.js
@@ -3,30 +3,42 @@ import {ADD_CLOCK, REMOVE_CLOCK} from "./actions";
 
 const defaultZone = 'Europe/Kiev';
 
+const zoneNames = moment.tz.names();
+
 const initialState = {
-    zonesList: moment.tz.names().filter(function(zone) {
-        return zone !== defaultZone;
-    }),
+    zonesList: remove(zoneNames, defaultZone),
     clocksList: [defaultZone]
 };
+
+function remove(array, ...elements) {
+    let tmpArray = array.slice();
+
+    for(let element of elements) {
+        const index = tmpArray.indexOf(element);
+
+        if (index > -1) {
+            tmpArray.splice(index, 1);
+        }
+    }
+
+    return tmpArray;
+}
 
 function clockApp(state = initialState, action) {
     switch (action.type) {
         case ADD_CLOCK:
             const clocksListAddedItem = state.clocksList.concat(action.timezone);
-            const zonesListRemovedItem = state.zonesList.filter(function(zone) {
-                return zone !== action.timezone;
-            });
+            const zonesListRemovedItem =  remove(state.zonesList, action.timezone);
+
             return Object.assign({}, state, {
                     clocksList: clocksListAddedItem,
                     zonesList: zonesListRemovedItem
                 }
             );
         case REMOVE_CLOCK:
-            const zonesListAddedItem = state.zonesList.concat(action.timezone);
-            const clocksListRemovedItem = state.clocksList.filter(function(zone) {
-                return zone !== action.timezone;
-            });
+            const clocksListRemovedItem = remove(state.clocksList, action.timezone);
+            const zonesListAddedItem = remove(zoneNames, ...clocksListRemovedItem);
+
             return Object.assign({}, state, {
                     clocksList: clocksListRemovedItem,
                     zonesList: zonesListAddedItem
